5025571e5d5e76bfe384b682d2e92b92fe7efaed
Correu <tylercorreu@yahoo.com>
2021-07-23 12:14:16 -0500

    romanToInt addition

---
+++ src/romanToInt.java
@@ -0,0 +1,13 @@
+public class romanToInt {
+    public int romanToInt(String s) {
+        //if the roman value is a single digit string, return its value
+        if(s.length() == 1) {
+
+        }
+        //else iterate through the String to determine the values
+        for(int i = 0; i < s.length(); i++) {
+
+        }
+        return 0;
+    }
+}
