29eda8c3a48627e3b393ce8ad4147d4e484eb30c
Correu <tylercorreu@yahoo.com>
2021-07-27 10:39:32 -0500

    07-27-2021 1039

--- src/subWithoutRepeatingCharacters.java
+++ src/subWithoutRepeatingCharacters.java
@@ -2,7 +2,19 @@
     find the length of the longest substring without repeating characters
  */
 public class subWithoutRepeatingCharacters {
-    public int lengthOfLongestSubstring(String s) {
-        return 0;
+    public static int lengthOfLongestSubstring(String s) {
+        int charCount = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != s.charAt(i + 1) && (i+1) < s.length()) {
+                charCount++;
+            }
+        }
+        return charCount;
+    }
+
+    public static void main(String[] args) {
+        String testString = "abcbcdcd";
+        System.out.println(lengthOfLongestSubstring(testString));
+        System.out.println("test");
     }
 }
