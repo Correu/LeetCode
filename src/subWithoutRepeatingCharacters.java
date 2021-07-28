c2761c0b21f13f690e62954ab68d8a011bbc99af
Tyler Correu <60193729+Correu@users.noreply.github.com>
2021-07-27 19:50:08 -0500

    changes to lengthoflongestsubstring method

--- src/subWithoutRepeatingCharacters.java
+++ src/subWithoutRepeatingCharacters.java
@@ -4,17 +4,26 @@
 public class subWithoutRepeatingCharacters {
     public static int lengthOfLongestSubstring(String s) {
         int charCount = 0;
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) != s.charAt(i + 1) && (i+1) < s.length()) {
+        String temp = "";
+        for(int i = 0; i < s.length()-1; i++) {
+            System.out.println(s.charAt(i) + " " + s.charAt(i + 1));
+            String currValue = "" + s.charAt(i);
+            if(!temp.contains(currValue)) {
+                temp += s.charAt(i);
+                System.out.println(temp);
                 charCount++;
+            } else {
+                break;
             }
         }
         return charCount;
     }
 
     public static void main(String[] args) {
-        String testString = "abcbcdcd";
-        System.out.println(lengthOfLongestSubstring(testString));
-        System.out.println("test");
+        String testString = "ababa";
+        String test2 = "bbbbb";
+        String s = "abcabcbb";
+        String p = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(p));
     }
 }
