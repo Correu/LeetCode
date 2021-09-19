72bad1d800b601d384ae2ec759b029324c9742ea
Tyler Correu <60193729+Correu@users.noreply.github.com>
2021-09-18 22:34:25 -0500

    changes to repeating char problem

--- src/subWithoutRepeatingCharacters.java
+++ src/subWithoutRepeatingCharacters.java
@@ -1,17 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
     find the length of the longest substring without repeating characters
  */
 public class subWithoutRepeatingCharacters {
     //parses through the length of the string
     public static int lengthOfLongestSubstring(String s) {
-        System.out.println(s.length());
+        Map<Character, Integer> word = new HashMap<>();
         int count = 0;
-        String currSub = "";
-        for(int i = 0; i < s.length(); i++) {
-            String currChar = "" + s.charAt(i);
-            currSub = currSub + currChar;
-            System.out.println(currSub + " " + currChar);
-            if(currSub.contains(currChar)) {
+        for (int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+            if (word.containsKey(currChar)) {
                 count++;
             } else {
                 break;
@@ -26,11 +26,13 @@ public class subWithoutRepeatingCharacters {
     }
 
     public static void main(String[] args) {
-        String s = "abcabcbb";
-        String p = "pwwkew";
-        String b = "bbbbbbb";
         String blank = "";
-        System.out.println("pwwkew\t" + lengthOfLongestSubstring(p));
+        String one = "abcabcbb";
+        String two = "pwwkew";
+        String three = "bbbbbbb";
+        System.out.println("abcabcbb\t" + lengthOfLongestSubstring(one));
+        System.out.println("pwwkew\t" + lengthOfLongestSubstring(two));
+        System.out.println("bbbbbbb\t" + lengthOfLongestSubstring(three));
         System.out.println("blank\t" + lengthOfLongestSubstring(blank));
     }
 }
