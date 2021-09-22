2b8758c5217c3fdfb65ad3613c28f39842a4af0a
Tyler Correu <60193729+Correu@users.noreply.github.com>
2021-09-22 07:24:13 -0500

    changes to validParenthesis

--- src/validParenthesis.java
+++ src/validParenthesis.java
@@ -4,8 +4,26 @@ public class validParenthesis {
     public static boolean validParenthesis(String s) {
         Stack<String> paren = new Stack<>();
         for(int i = 0; i < s.length(); i++) {
+            System.out.println(String.valueOf(s.charAt(i)));
             if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
                 paren.push(String.valueOf(s.charAt(i)));
+            else if(s.charAt(i) == ')' && paren.peek() == "(") {
+                System.out.println(paren.peek());
+                paren.pop();
+                return true;
+            }
+            else if(s.charAt(i) == ']' && paren.peek() == "[") {
+                System.out.println(paren.peek());
+                paren.pop();
+                return true;
+            }
+            else if(s.charAt(i) == '}' && paren.peek() == "{") {
+                System.out.println(paren.peek());
+                paren.pop();
+                return true;
+            }
+            else
+                return false;
         }
         return true;
     }
@@ -16,5 +34,7 @@ public class validParenthesis {
         String three = "({})";
 
         System.out.println(validParenthesis(one));
+        System.out.println(validParenthesis(two));
+        System.out.println(validParenthesis(three));
     }
 }
