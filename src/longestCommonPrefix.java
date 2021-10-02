5fb18575332060c3b3d1060b3a77cdf3361956ec
Tyler Correu <60193729+Correu@users.noreply.github.com>
2021-10-02 07:54:36 -0500

    adding longestcommmonprefix problem

--- src/longestCommonPrefix.java
+++ src/longestCommonPrefix.java
@@ -2,13 +2,23 @@
     Problem 14
  */
 public class longestCommonPrefix {
-    public String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefix(String[] strs) {
         if(strs.length == 0) return "";
         String output = strs[0];
         for(int i = 0; i < strs.length; i++) {
-
+            System.out.println(strs[i] + " " + i);
+            while(strs[i].indexOf(output) != 0) {
+                output = output.substring(0, output.length() - 1);
+                System.out.println(output);
+                System.out.println(strs[i].indexOf(output));
+                if(output.isEmpty()) return "";
+            }
         }
-        while()
-        return "";
+        return output;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(strs));
     }
 }
